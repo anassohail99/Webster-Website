b35efdfe1c1054a4e719bbb3ceeadc19fecc3075
anassohail99 <anas.sohailabs1@gmail.com>
2020-05-29 17:52:07 +0500

    work js file

--- Js/work.js
+++ Js/work.js
@@ -1,3 +1,20 @@
+// $(document).ready(function () {
+//   $grid = $(".portfolio-item").isotope({
+//     // options
+//     itemSelector: ".item",
+//     layoutMode: "masonry",
+//     masonry: {
+//       gutter: 0,
+//     },
+//   });
+//   // filter items on button click
+//   $(".portfolio-ul").on("click", "li", function () {
+//     var filterValue = $(this).attr("data-filter");
+//     $grid.isotope({ filter: filterValue });
+//     $(".portfolio-ul li").removeClass("active");
+//     $(this).addClass("active");
+//   });
+// });
 // ========================================================================= //
 //  Porfolio isotope and filter
 // ========================================================================= //
