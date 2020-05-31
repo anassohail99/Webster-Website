30a7d379dfa97da372d65554d163d5fcd08264f8
anassohail99 <anas.sohailabs1@gmail.com>
2020-05-31 20:56:08 +0500

    JS

--- Js/preloader.js
+++ Js/preloader.js
@@ -13,6 +13,45 @@ function stopPreLoader() {
 
 setTimeout(stopPreLoader, 2000);
 
+$(document).ready(function () {
+  $(".button a").click(function () {
+    $(".overlay").fadeToggle(200);
+    $(this).toggleClass("btn-open").toggleClass("btn-close");
+  });
+});
+$(".overlay").on("click", function () {
+  $(".overlay").fadeToggle(200);
+  $(".button a").toggleClass("btn-open").toggleClass("btn-close");
+  open = false;
+});
+
+var navbar = document.querySelector(".Nav");
+
 window.onscroll = function () {
-  myFunction();
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 50) {
+    navbar.classList.add("sticky");
+    navbar.style.background = "#1D3E9E";
+  } else {
+    navbar.classList.remove("sticky");
+    navbar.style.background = "transparent";
+  }
 };
+// ===== Scroll to Top ====
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 50) {
+    // If page is scrolled more than 50px
+    $("#return-to-top").fadeIn(200); // Fade in the arrow
+  } else {
+    $("#return-to-top").fadeOut(200); // Else fade out the arrow
+  }
+});
+$("#return-to-top").click(function () {
+  // When arrow is clicked
+  $("body,html").animate(
+    {
+      scrollTop: 0, // Scroll to top of body
+    },
+    10
+  );
+});
